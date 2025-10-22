848769e5ef83361d20c0758d8789074289ba1c83
nivds08 <niveditasinha0811@gmail.com>
2025-10-22 15:20:09 +0530

    Last updates

--- src/data_handler/fetch_manager.py
+++ src/data_handler/fetch_manager.py
@@ -10,15 +10,17 @@ if project_root not in sys.path:
     sys.path.append(project_root)
 
 # Import local modules
+
+
 from src.data_handler.preprocessor import suggest_columns
-from src.data_handler.api_handler import fetch_data_using_resource_id
+from src.data_handler.api_handler import fetch_data
 
 def fetch_from_api(resource_id):
     """
     Fetch dataset from data.gov.in using resource_id and suggest columns.
     Returns: DataFrame, suggestions
     """
-    df = fetch_data_using_resource_id(resource_id)
+    df = fetch_data(resource_id)
     if df is not None and not df.empty:
         suggestions = suggest_columns(df)
         return df, suggestions
